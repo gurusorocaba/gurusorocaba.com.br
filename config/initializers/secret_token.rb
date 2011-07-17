fe68664afd1729f5d85fd824baf63560ad629329
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2011-07-17 20:17:34 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GurusorocabaComBr::Application.config.secret_token = '7be6df65b1e602261867c7fbf0599680705e75e28e11b25cd5da17300a61ae764d012302a20b0031c7a2d778e2b05a0f4d36b6f464d91303becbdad3443a9644'
