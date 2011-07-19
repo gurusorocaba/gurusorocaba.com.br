2ba1cf972e654dfc6153185424729ff2c15ff718
Lucas Renan <lucas@freelancersbrasil.com>
2011-07-18 23:36:24 -0300

    mongoid and rspec configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.full_backtrace = true
+  config.mock_with :rspec
+
+  # config.include Devise::TestHelpers, :type => :controller
+  # config.extend ControllerMacros, :type => :controller
+
+  config.before(:suite) do
+    DatabaseCleaner.orm = :mongoid
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
