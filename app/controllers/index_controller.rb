df30d512a29f77ea4c80670ed81d0377bb3e921c
Lucas Renan <lucas@freelancersbrasil.com>
2011-08-03 01:30:56 -0300

    list agenda - front end

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,7 @@
 class IndexController < ApplicationController
   def index
+    # @agendas = Agenda.where(:date.gte => Date.today).desc(:date)
+    @agendas = Agenda.desc(:date)
   end
 
 end
--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe IndexController do
+  
+  def agenda_attributes
+    Factory.attributes_for(:agenda)
+  end
 
   describe "GET 'index'" do
     it "should be successful" do
-      get 'index'
+      agenda = Agenda.create! agenda_attributes
+      get :index
+      assigns(:agendas).should eq([agenda])
       response.should be_success
     end
   end
