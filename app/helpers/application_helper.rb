5ad213bdccd95c9190b1cdc93c9d8cbbec8dd376
Lucas Renan <lucas@freelancersbrasil.com>
2012-02-26 01:57:41 -0300

    sets 'head up grid js' to verify if elements are 'inside' the grids

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def development_assets
+    assets_path = "#{Rails.root}/vendor/assets/development/"
+    
+    [stylesheet_link_tag(assets_path + "stylesheets/hugrid.css"),
+    javascript_include_tag(assets_path + "javascripts/hugrid.js")].join.html_safe
+  end
 end
