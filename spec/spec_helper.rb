b35a89e872cd893642bf20981dae46e422e423d9
lucas renan <contato@lucasrenan.com>
2013-04-13 02:57:16 -0300

    trying to configure coveralls for this rails application with simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,26 @@
-require 'coveralls'
-Coveralls.wear!
+require "simplecov"
+require "coveralls"
 
-require 'simplecov'
-SimpleCov.start('rails') do
-  add_filter '/vendor/'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start "rails" do
+  add_filter "/vendor/"
 end
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+# require "coveralls"
+# Coveralls.wear! "rails"
+
+# require "simplecov"
+# SimpleCov.start "rails" do
+#   add_filter "/vendor/"
+# end
+
+# This file is copied to spec/ when you run "rails generate rspec:install"
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require "rspec/rails"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
