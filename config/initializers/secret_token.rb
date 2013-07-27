a08000a94777e98a41aa6c55df2255edc381966c
lucas renan <contato@lucasrenan.com>
2013-07-26 21:38:50 -0300

    removing secrete token from git

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gurusorocaba::Application.config.secret_token = '05171cd5f1c1010fee22b57540fece2f20c87e99714e06de8cd1350aa617a38fa18e7677e4f11252d6747d546148e69b9da24321f67abb59399fc2ea47e83678'
