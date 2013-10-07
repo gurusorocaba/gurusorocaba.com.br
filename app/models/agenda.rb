e821e65d949b3c471184f4f91cc919e1d8fc329e
lucas renan <contato@lucasrenan.com>
2013-10-06 22:58:33 -0300

    bug fix - use lambda for scopes

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -3,21 +3,21 @@ class Agenda
   include Mongoid::Timestamps
   include Mongoid::MultiParameterAttributes
   include Mongoid::Slug
-  
+
   field :title
   field :description
   field :type
   field :date, :type => DateTime
   field :place
-  
+
   TYPES = %W( course dojo event hack_day meeting mentoring )
-  
+
   validates_presence_of :title, :date, :place
   validates_inclusion_of :type, :in => TYPES
 
-  scope :future, where(:date.gte => Date.today).desc(:date)
-  scope :past, where(:date.lte => Date.today).desc(:date)
-  
+  scope :future, -> { where(:date.gte => Date.today).desc(:date) }
+  scope :past,  -> { where(:date.lte => Date.today).desc(:date) }
+
   slug :title
 
   index({ date: 1 })
